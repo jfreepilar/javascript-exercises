83c6c730b3a505503b9e5b33c4fe9dc73d1209b2
jreepilar <tipsjsp@gmail.com>
2024-01-23 23:44:41 +0800

    COMPLETE all the exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else {
+        const higherNum = Math.max(num1, num2);
+        const lowerNum = Math.min(num1, num2);
+        const range =  (higherNum - lowerNum) + 1;
+        const total = [];
 
+        for (let i = 0; i < range; i++) {
+            total.push(lowerNum + i);
+        }
+
+        const sum = total.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+        }, 0);
+
+            return sum;
+        } 
 };
 
 // Do not edit below this line
