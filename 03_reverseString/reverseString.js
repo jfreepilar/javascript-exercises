c62327e3aeca2a5d5fb3fca7fc9e9b1d93550ac5
jreepilar <tipsjsp@gmail.com>
2024-01-23 23:41:07 +0800

    COMPLETE all the exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedString = splitString.reverse();
+    let joinedString = reversedString.join("");
+    return joinedString;
 };
 
 // Do not edit below this line
