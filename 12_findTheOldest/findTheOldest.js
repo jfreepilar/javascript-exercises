f2bb0bec59950e26c84b9bfad6610b34ba63071b
jreepilar <tipsjsp@gmail.com>
2024-02-16 08:20:16 +0800

    COMPLETE all the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    checkIfStillAlive(people);
+    for (let i = 0; i < people.length; i++) {
+        let yearsLived = people[i].yearOfDeath - people[i].yearOfBirth;
+        people[i].age = `${yearsLived}`;
+    }
+        people.sort((a, b) => b.age - a.age);
+        return people[0];
 };
+
+function checkIfStillAlive(people) {
+    for (let i = 0; i < people.length; i++) {
+    if (!people[i].hasOwnProperty('yearOfDeath')) {
+        let currentYear = parseInt((new Date()).getFullYear()); console.log(currentYear)
+        people[i].yearOfDeath = `${currentYear}`; console.log(people)
+        }
+    }
+}    
 
 // Do not edit below this line
 module.exports = findTheOldest;
