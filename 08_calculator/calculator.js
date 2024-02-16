39dc1aa6287cb86d3dc42f09f241a5a361e91a25
jreepilar <tipsjsp@gmail.com>
2024-02-16 08:17:18 +0800

    COMPLETE all the exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  let sum = number1 + number2;
+  return sum;
+}
+
+const subtract = function(number1, number2) {
+	let difference = number1 - number2;
+  return difference;
 };
 
-const subtract = function() {
-	
+const sum = function(array=[]) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+  let result = array.reduce((a, b) => a + b)
+  return result;
+  }
 };
 
-const sum = function() {
-	
+const multiply = function(array=[]) {
+  let result = array.reduce((a, b) => a * b)
+  return result;
 };
 
-const multiply = function() {
-
+const power = function(number1, number2) {
+  let result = Math.pow(number1, number2)
+        return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = [];
+        if (n == 0 || n == 1)
+            return 1;
+        if (f[n] > 0)
+            return f[n];
+        return f[n] = factorial(n-1) * n;
 };
 
 // Do not edit below this line
